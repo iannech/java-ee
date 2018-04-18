c9f43cae3d672cf30e659f24144ff44fbc9cce68
iannech <ianoniango94@gmail.com>
2018-04-18 21:06:25 +0300

    bank-webapp skeleton

---
+++ bank-webapp/src/main/java/com/org/bankwebapp/BankWebappApplication.java
@@ -0,0 +1,12 @@
+package com.org.bankwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebappApplication.class, args);
+	}
+}
