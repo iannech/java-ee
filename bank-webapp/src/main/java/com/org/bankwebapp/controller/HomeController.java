a8aa7b9e213e5361fe7c1a00c9fbb0a8094dce3a
iannech <ianoniango94@gmail.com>
2018-04-22 19:50:31 +0300

    add signup component

--- bank-webapp/src/main/java/com/org/bankwebapp/controller/HomeController.java
+++ bank-webapp/src/main/java/com/org/bankwebapp/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.org.bankwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.org.bankwebapp.domain.User;
 
 @Controller
 public class HomeController {
@@ -14,5 +19,21 @@ public class HomeController {
 	@RequestMapping("/index")
 	public String index() {
 		return "/index";
+	}
+	
+	// http GET call to retrieve signup page
+	@RequestMapping(value = "/signup", method=RequestMethod.GET)
+	public String signup(Model model) {
+		User user = new User();
+		
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+	
+	// method to do sign up
+	@RequestMapping(value="signup", method=RequestMethod.POST)
+	public void signupPost(@ModelAttribute("user") User user, Model model) {
+		
 	}
 }
